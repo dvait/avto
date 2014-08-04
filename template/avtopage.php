361ee65bdc8b53d0e6e488f248b3b17032ba1fcc
Dvait <>
2014-08-04 18:29:48 +0400

    Merge origin/master

--- template/avtopage.php
+++ template/avtopage.php
@@ -2,19 +2,37 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Главная страница</title>
+        <title>Объявление №<?php echo $id;?></title>
     </head>
     <body>
         <?php
 
+        $avtocolors = "";
+        $memrow = null;
         
-        if ($row = $ret->fetch())
-            {
+        while ($row = $ret->fetch())
+        {
+            $avtocolors .= $row->color . ", ";
+            
+            if (!$memrow) { $memrow = $row; }
+            
+        }
+        
+        if ($memrow) {
 
-                printf('<div><img src="%s" width=\'720\' height=\'540\'><br>Авто (марка, модель): %s %s<br>Тип кузова: %s<br>Цена: %s руб.<br>Описание: %s<br></div>',
-                        $this->getPhotoName($row->photoname, avto::PhotoSizeAvtoPage), $row->brand, $row->model, $row->typecarbody, $row->price, $row->description);
+            $avtocolors = trim($avtocolors,", ");
+            if ($avtocolors == "") {
+                $avtocolors = "Не указаны";
             }
+            
+            printf('<div><img src="%s" width=\'720\' height=\'540\'><br>Авто (марка, модель): %s %s<br>Тип кузова: %s<br>Цвета: %s<br>Цена: %s руб.<br>Описание: %s<br></div>',
+               $this->checkPhoto($memrow->photoname, avto::PhotoSizeAvtoPage), $memrow->brand, $memrow->model, $memrow->typecarbody, $avtocolors, $memrow->price, $memrow->description);
+        } else {
+            echo "Объявление удалено.";
+        }
+        
         ?>
+
         <br><br><div><a href="index.php">Перейти на главную страницу</a></div>
 
     </body>
